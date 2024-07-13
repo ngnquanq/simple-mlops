bebd7e7c08e3350e9e63edc578e7ef6d50b6c146
Ng. Nhật Quang <101655590+ngnquanq@users.noreply.github.com>
2024-07-13 23:42:51 +0700

    Adding evaluate model

--- src/steps/processing/postprocessing.py
+++ src/steps/processing/postprocessing.py
@@ -1,9 +1,12 @@
 import pandas as pd 
 import numpy as np 
 from abc import ABC, abstractmethod
+from sklearn.metrics import *
 
 class Evaluate(ABC):
     """
     Use this class to output the result from the predict.
     """
-    
+    @abstractmethod
+    def evaluate_metric(self, metric: str):
+        pass 
