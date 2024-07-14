c43d89a835903d8b240c8d6cea387ee2774e022a
Ng. Nhật Quang <101655590+ngnquanq@users.noreply.github.com>
2024-07-14 13:10:42 +0700

    Adjust
    Adjust the evaluation code

--- src/steps/evaluate.py
+++ src/steps/evaluate.py
@@ -1,7 +1,27 @@
 from zenml import step
 import pandas as pd
 import logging 
+from typing import Union, Tuple, Annotated
+from models.evaluate_model import *
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+logging.basicConfig(filename='logs/evaluate.log')
+logger = logging.getLogger(__name__)
 
 @step
-def evaluate_model(df: pd.DataFrame) -> None: 
-    pass
+def evaluate_model(
+    model: ClassifierMixin,
+    feature: Union[pd.DataFrame | pd.Series | np.ndarray],
+    target: Union[pd.DataFrame | pd.Series] | np.ndarray
+    ) -> Tuple[
+        Annotated[dict, "Confusion matrix"] ,
+        Annotated[dict, "Other metrics"]
+        ]: 
+    prediction = model.predict(feature)
+    cm_class = ConfusionMatrix()
+    cm = cm_class.calculate_score(y_true=target, y_pred=prediction)
+
+    metric_class = ClassificationMetrics()
+    metrics = metric_class.calculate_score(y_true=target, y_pred=prediction)
+    
+    return cm, metrics
--- src/steps/models/evaluate_model.py
+++ src/steps/models/evaluate_model.py
@@ -21,12 +21,17 @@ class Evaluation(ABC):
 class ConfusionMatrix(Evaluation):
     """Evaluattion Strategy that used Confusion Matrix
     """
-    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+    def calculate_score(self, y_true: np.ndarray, y_pred: np.ndarray) -> Dict[str , np.ndarray]:
         try: 
+            # Convert DataFrame or Series to NumPy array
+            if isinstance(y_true, (pd.DataFrame, pd.Series)):
+                y_true = y_true.to_numpy().ravel()
+            if isinstance(y_pred, (pd.DataFrame, pd.Series)):
+                y_pred = y_pred.to_numpy().ravel()
             logger.info('Calculating Confusion matrix')
-            cm = confusion_matrix(y_true=y_true, y_pred=y_pred)
+            cm = confusion_matrix(y_true=y_true.argmax(axis=1), y_pred=y_pred.argmax(axis=1))
             logger.info('Calculate confusion matrix complete')
-            return cm
+            return {'Confusion matrix': cm}
         except Exception as e: 
             logging.error("Error in calculating Confusion matrix: {}".format(e))
             raise e 
@@ -46,9 +51,9 @@ class ClassificationMetrics(Evaluation):
             
             logger.info('Calculating other metrics')
             accuracy = accuracy_score(y_true, y_pred)
-            f1 = f1_score(y_true, y_pred)
-            recall = recall_score(y_true, y_pred)
-            precision = precision_score(y_true, y_pred)
+            f1 = f1_score(y_true, y_pred, average='micro')
+            recall = recall_score(y_true, y_pred, average='micro')
+            precision = precision_score(y_true, y_pred, average='micro')
             logger.info('Calculate metrics complete')
             return {
                 'accuracy': accuracy, 
